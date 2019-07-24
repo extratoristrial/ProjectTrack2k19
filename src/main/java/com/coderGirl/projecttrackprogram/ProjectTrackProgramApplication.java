5eb32a031260dc392851e7a9ecc447bdb1503eca
Tori Stake <v.m.stake@gmail.com>
2019-07-24 18:35:06 -0500

    commiting new repository

---
+++ src/main/java/com/coderGirl/projecttrackprogram/ProjectTrackProgramApplication.java
@@ -0,0 +1,13 @@
+package com.coderGirl.projecttrackprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTrackProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTrackProgramApplication.class, args);
+	}
+
+}
